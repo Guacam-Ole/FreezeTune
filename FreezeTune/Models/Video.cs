5b79ad8d99cff38ba749d76929bff0931008b1d7
Ole Albers <ole@nocarrier.de>
2026-01-17 16:06:17 +0100

    VideoModel

---
+++ FreezeTune/Models/Video.cs
@@ -0,0 +1,10 @@
+namespace FreezeTune.Models;
+
+public class Video
+{
+    public string Url { get; set; }
+    public List<int> ImageIds { get; set; }
+    public string Interpret { get; set; }
+    public string Title { get; set; }
+    public DateOnly Date { get; set; }
+}
