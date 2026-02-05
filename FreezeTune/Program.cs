48d4063522581ef8a52cfd919bda546df394fa07
Ole Albers <ole@nocarrier.de>
2026-02-05 21:10:16 +0100

    Serilog

--- FreezeTune/Program.cs
+++ FreezeTune/Program.cs
@@ -1,8 +1,13 @@
+using System.Reflection;
 using FreezeTune;
 using Microsoft.Extensions.Options;
 using FreezeTune.Logic;
 using FreezeTune.Repositories;
 using FreezeTune.Services;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Grafana.Loki;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +23,27 @@ builder.Services.AddScoped<IImageRepository, ImageRepository>();
 builder.Services.AddScoped<IVideoRepository, VideoRepository>();
 builder.Services.AddScoped<IMaintenanceLogic, MaintenanceLogic>();
 builder.Services.AddSingleton<ProgressService>();
+builder.Services.AddLogging(cfg => cfg.SetMinimumLevel(LogLevel.Debug));
+builder.Services.AddSerilog(cfg =>
+{
+    cfg.MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("job", Assembly.GetEntryAssembly()?.GetName().Name)
+        .Enrich.WithProperty("desktop", Environment.GetEnvironmentVariable("DESKTOP_SESSION"))
+        .Enrich.WithProperty("language", Environment.GetEnvironmentVariable("LANGUAGE"))
+        .Enrich.WithProperty("lc", Environment.GetEnvironmentVariable("LC_NAME"))
+        .Enrich.WithProperty("timezone", Environment.GetEnvironmentVariable("TZ"))
+        .Enrich.WithProperty("dotnetVersion", Environment.GetEnvironmentVariable("DOTNET_VERSION"))
+        .Enrich.WithProperty("inContainer", Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"))
+        .WriteTo.GrafanaLoki(Environment.GetEnvironmentVariable("LOKIURL") ?? "http://thebeast:3100",
+            propertiesAsLabels: ["job"]);
+    if (Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyConfigurationAttribute>()?.Configuration ==
+        "Debug")
+    {
+        cfg.WriteTo.Console(new RenderedCompactJsonFormatter());
+    }
+});
 
 var app = builder.Build();
 
